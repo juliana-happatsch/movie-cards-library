d49d8c8e71501b6ea0ce8c4b3a0818627a89b997
Juliana Happatsch <jhappatsch@gmail.com>
2021-05-27 02:16:59 -0300

    Updates

---
+++ precache-manifest.ee16c7932567c5b8cb3798f8ba197b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4fa4b1443fa9a99d7ce42faa86ffc9",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "2362c7af33b6858b32ea",
+    "url": "/movie-cards-library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "24e1e214416ac9376820",
+    "url": "/movie-cards-library/static/js/2.59a3b2c7.chunk.js"
+  },
+  {
+    "revision": "2362c7af33b6858b32ea",
+    "url": "/movie-cards-library/static/js/main.5e7cffb9.chunk.js"
+  },
+  {
+    "revision": "a95db5ba2439b13bdc08",
+    "url": "/movie-cards-library/static/js/runtime-main.a37d5318.js"
+  }
+]);
